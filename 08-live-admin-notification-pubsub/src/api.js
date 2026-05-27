a4859b9e7ce8d459b9036be1baa682688522d024
MOHIT <mohitjyouti2003@gmail.com>
2026-05-27 17:02:21 +0530

    add files

---
+++ 08-live-admin-notification-pubsub/src/api.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const Redis = require("ioredis");
+const publisher = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
+const app = express();
+
+app.use(express.json());
+
+app.post("/notifications", async (req, res) => {
+  const payload = {
+    title: req.body.title,
+    createdAt: new Date().toISOString(),
+  };
+  const receivers = await publisher.publish(
+    "notifications",
+    JSON.stringify(payload),
+  );
+
+  res.status(200).json({
+    message: `Notification sent to ${receivers} subscribers!`,
+  });
+});
+
+app.listen(8528, () => {
+  console.log("Server is running on port 8528");
+});
---
+++ 08-live-admin-notification-pubsub/src/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const Redis = require("ioredis");
+
+const subscriber = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
+
+subscriber.subscribe("notifications", (err) => {
+  if (err) {
+    console.error("Failed to subscribe: %s", err.message);
+    return;
+  } else {
+    console.log(`Subscribed successfully!.`);
+  }
+});
+
+subscriber.on("message", (channel, message) => {
+  console.log(`Received message from ${channel}: ${JSON.parse(message)}`);
+});
+
+const app = express();
+app.use(express.json());
+
+app.listen(8529, () => {
+  console.log("Server is running on port 8529");
+});
